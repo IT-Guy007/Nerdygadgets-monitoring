57855b561c90dce46546257ea5149b65007f8b30
Lovis1234 <90246498+Lovis1234@users.noreply.github.com>
2022-05-09 13:59:33 +0200

    methods en firewall final

--- src/main.java
+++ src/main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+import java.io.Serial;
+
 public class main {
     public static void main(String[] args) {
+        JPanel panel = new JPanel();
+        servers a = new WebServer(panel, "jemoeder", 69, true,12,12);
+        System.out.println(a);
+        System.out.println(a.getPrijs());
 
     }
 }
