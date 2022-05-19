bb84b507dfd27744aac30de41e9754f04c3be478
Jeroen <76699269+IT-Guy007@users.noreply.github.com>
2022-05-19 13:04:00 +0200

    GetProjectID

--- src/nerdygadgets/Monitoring/MonitoringFrame.java
+++ src/nerdygadgets/Monitoring/MonitoringFrame.java
@@ -1,10 +1,13 @@
 package nerdygadgets.Monitoring;
 
+import com.mysql.cj.jdbc.exceptions.CommunicationsException;
 import nerdygadgets.Design.components.ServerDragAndDrop;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class MonitoringFrame extends JFrame implements ActionListener {
     private int maxiumservercount;
@@ -14,7 +17,7 @@ public class MonitoringFrame extends JFrame implements ActionListener {
     int schermbreedte = schermgrootte.width;
     // private paneel MonitoringPanel;
     public MonitoringFrame(int projectID) {
-        setTitle("Design");
+        setTitle("Monitoring van " + getProjectName(projectID));
         setLayout(new FlowLayout());
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(schermbreedte/30*26,schermhoogte/30*26); //Maakt de groote van de gui de helft van de schermgrootte
@@ -28,5 +31,47 @@ public class MonitoringFrame extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public String getProjectName(int i) {
+        Connection connection = null;
+        PreparedStatement p = null;
+        ResultSet rs = null;
+        ArrayList<String> output = new ArrayList<String>();
+        String output2 = null;
+        try {
+            // Importing and registering drivers
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con = DriverManager.getConnection("jdbc:mysql:/windesheim.app:3306/application", "group4", "Qwerty1@");
+
+            // SQL command data stored in String datatype
+            String sql = "select * from project where ID = " + i;
+            p = con.prepareStatement(sql);
+            rs = p.executeQuery();
+
+            // In array zetten;
+            while (rs.next()) {
+                output.set(rs.getInt("ID"),rs.getString("naam"));
+            }
+
+
+        } catch (CommunicationsException ce) {
+            JLabel error = new JLabel("Error, kan niet verbinden met de server");
+            error.setVisible(true);
+            error.repaint();
+            add(error);
+
+        } catch (SQLException e) {
+            System.out.println(e);
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+
+        }
+
+        output2 = output.get(0);
+
+        return output2;
     }
 }
