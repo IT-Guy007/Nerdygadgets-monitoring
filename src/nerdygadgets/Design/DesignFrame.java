689f2e7267b8464d4186b5c3cfb986a07f946aaf
bramteunis <51489074+bramteunis@users.noreply.github.com>
2022-05-31 14:51:38 +0200

    opslaan systeem werkend

--- src/nerdygadgets/Design/DesignFrame.java
+++ src/nerdygadgets/Design/DesignFrame.java
@@ -476,33 +476,77 @@ public class DesignFrame extends JFrame implements ActionListener {
     }
 
     public void save(){
-        String setupID = "jevader"; //TODO Via dialoog ff hier een unique "filename meegeven"
+        String setupID = "jevader4"; //TODO Via dialoog ff hier een unique "filename meegeven"
         boolean unique = true;
         try {
             Connection conn = DriverManager.getConnection(
                     "jdbc:mysql://192.168.1.103:3306/application",
                     "group4", "Qwerty1@");
             Statement stmt = conn.createStatement();
-            String uniqueQuery = "SELECT setupId from serverSetups";
+            String uniqueQuery = "SELECT name from project";
             ResultSet rset = stmt.executeQuery(uniqueQuery);
-
+            System.out.println("test1");
             while(rset.next()) {
-                if (rset.getString("setupID").equals(setupID)) {
+                if (rset.getString("name").equals(setupID)) {
+                    System.out.println("test2");
                     unique = false;
                 }
             }
-            if (unique) {
+
+
+             if (unique) {
+                 String query5 = "INSERT INTO project(name,wanted_Availability) VALUES ('"+setupID+"',100.0)";
+                 stmt.executeUpdate(query5);
+                 System.out.println("test3");
+                 String query3 = "SELECT projectID from project where name = '"+setupID+"'";
+                 ResultSet rset3 = stmt.executeQuery(query3);
+                 String projectID="0";
+                 while(rset3.next()) {
+                     projectID = rset3.getString("projectID");
+                 }
+                for (ServerOptie opties: tempServerOpties){
+                    try {
+                        if (opties.getType().equals("webserver")) {
+                            String query2 = "INSERT INTO serverSetups(name,type,price,availability,setupID) VALUES ('" + opties.getName() + "','webserver'," + opties.getPrijs() + "," + opties.getBeschikbaarheid() + ",'"+setupID+"')";
+                            stmt.executeUpdate(query2);
+                        } else if (opties.getType().equals("databaseserver")) {
+                            String query2 = "INSERT INTO serverSetups(name,type,price,availability,setupID) VALUES ('" + opties.getName() + "','databaseserver'," + opties.getPrijs() + "," + opties.getBeschikbaarheid() + ",'"+setupID+"')";
+                            stmt.executeUpdate(query2);
+                        } else if (opties.getType().equals("firewall")) {
+                            String query2 = "INSERT INTO serverSetups(name,type,price,availability,setupID) VALUES ('" + opties.getName() + "','firewall'," + opties.getPrijs() + "," + opties.getBeschikbaarheid() + ",'"+setupID+"')";
+                            stmt.executeUpdate(query2);
+                        }
+                    }catch (Exception e){}
+                }
                     for (ServerDragAndDrop server : designpanel.getServersArray_ArrayList()){
                         if (server instanceof WebServer) {
-                            String query = "INSERT INTO serverSetups(name, type, beschikbaarheid, prijs, setupId) VALUES ('" + server.getNaam() + "', '" + "webserver" + "', " + server.getBeschikbaarheid() + ", " + server.getPrijs() + ", +'" + setupID + "');";
+                            String query = "INSERT INTO servers(name, server_kindID, availability, price) VALUES ('" + server.getNaam() + "', 2, " + server.getBeschikbaarheid() + ", " + server.getPrijs() + ");";
                             stmt.executeUpdate(query);
                         } else if (server instanceof  DatabaseServer) {
-                            String query = "INSERT INTO serverSetups(name, type, beschikbaarheid, prijs, setupId) VALUES ('" + server.getNaam() + "', '" + "databaseserver" + "', " + server.getBeschikbaarheid() + ", " + server.getPrijs() + ", +'" + setupID + "');";
+                            String query = "INSERT INTO servers(name, server_kindID, availability, price) VALUES ('" + server.getNaam() + "',1, " + server.getBeschikbaarheid() + ", " + server.getPrijs() + ");";
                             stmt.executeUpdate(query);
                         } else if (server instanceof  Firewall) {
-                            String query = "INSERT INTO serverSetups(name, type, beschikbaarheid, prijs, setupId) VALUES ('" + server.getNaam() + "', '" + "firewall" + "', " + server.getBeschikbaarheid() + ", " + server.getPrijs() + ", +'" + setupID + "');";
+                            String query = "INSERT INTO servers(name, server_kindID, availability, price) VALUES ('" + server.getNaam() + "', 3, " + server.getBeschikbaarheid() + ", " + server.getPrijs() + ");";
                             stmt.executeUpdate(query);
                         }
+                        String uniqueQuery2 = "SELECT serverID from servers WHERE name = '"+server.getNaam()+"'";
+                        ResultSet rset2 = stmt.executeQuery(uniqueQuery2);
+                        String juiste_id="0";
+                        while(rset2.next()) {
+                            juiste_id = rset2.getString("serverID");
+                        }
+                        String query2 = "INSERT INTO server_Present(serverID, x,y) VALUES ("+juiste_id+","+server.getBounds().getX()+","+server.getBounds().getY()+")";
+                        stmt.executeUpdate(query2);
+
+                        String uniqueQuery4 = "SELECT server_PresentID from server_Present WHERE serverID = "+juiste_id;
+                        ResultSet rset7 = stmt.executeQuery(uniqueQuery4);
+                        String juiste_id2="0";
+                        while(rset7.next()) {
+                            juiste_id2 = rset7.getString("server_PresentID");
+                        }
+
+                        String query4 = "INSERT INTO project_Has_Servers(projectID,server_PresentID) VALUES ("+projectID+","+juiste_id2+")";
+                        stmt.executeUpdate(query4);
 
                     }
                 } else {System.out.println("niet uniek");}
