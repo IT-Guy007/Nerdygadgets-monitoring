f69e2fffe8a399a189e957136b34cae3e081c572
Skieja <twixgaming2000@gmail.com>
2022-05-31 11:05:16 +0200

    Beschikbaarheid en prijs stonden verkeerd

--- src/nerdygadgets/Design/components/ServerLists.java
+++ src/nerdygadgets/Design/components/ServerLists.java
@@ -6,12 +6,12 @@ public class ServerLists {
     private ArrayList<ServerDragAndDrop> servers = new ArrayList<>();
 
     public ServerLists() {
-        DatabaseServer ServerOptie1 = new DatabaseServer("WD10239",80,99.99);
-        DatabaseServer ServerOptie2 = new DatabaseServer("WD10240",85,130.4);
-        DatabaseServer ServerOptie3 = new DatabaseServer("WD10241",90,2200);
-        WebServer ServerOptie4 = new WebServer("HAL10239",80,99.99);
-        WebServer ServerOptie5 = new WebServer("HAL10240",85,130.4);
-        WebServer ServerOptie6 = new WebServer("HAL10241",90,2200);
+        DatabaseServer ServerOptie1 = new DatabaseServer("HAL9001DB",90,5100);
+        DatabaseServer ServerOptie2 = new DatabaseServer("HAL9002DB",95,7700);
+        DatabaseServer ServerOptie3 = new DatabaseServer("HAL9003DB",98,12200);
+        WebServer ServerOptie4 = new WebServer("HAL9001W",2200,80);
+        WebServer ServerOptie5 = new WebServer("HAL9002W",3200,90);
+        WebServer ServerOptie6 = new WebServer("HAL9003W",5100,95);
         servers.add(ServerOptie1);servers.add(ServerOptie2); servers.add(ServerOptie3);
         servers.add(ServerOptie4);servers.add(ServerOptie5);servers.add(ServerOptie6);
 
